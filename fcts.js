3aab85057cd3cd2c78024c7889f440b729d299ff
Joseph Herlant <herlantj@gmail.com>
2017-10-22 16:11:28 -0700

    Add button to re-runa job that is removed if no href on the child link elt

--- fcts.js
+++ fcts.js
@@ -1,16 +1,20 @@
 $(function() {
   $.get('index.json', function(datas) {
     parseChildren(datas);
-    console.log(datas)
     $.get('template-body.html', function(template){
       var rendered = Mustache.render(template, datas);
       $('#categories').html(rendered);
-    })
+      // This one removes the optional buttons that don't have href
+      $('#categories').find('.optional-btn').each(function() {
+        if($(this).find('a').attr('href') == ""){
+          $(this).remove();
+        }});
+    });
     $.get('template-toc.html', function(template){
       var rendered = Mustache.render(template, datas);
       $('#top-toc').html(rendered);
-    })
-  })
+    });
+  });
 });
 
 function parseChildren(datas){
